ecce6c4ec5aa8bb5e484a2dbe09232fc15e90399
Nacho Garcia <nash.makineta@gmail.com>
2015-10-26 18:24:35 +0000

    Image changes by changing an index number...

--- public_html/JavaScript/tutorial.js
+++ public_html/JavaScript/tutorial.js
@@ -3,23 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var imglinks = document.getElementById('imglinks').getElementsByTagName('a');
-var selectedImg;
+/*var imglinks = document.getElementById('imglinks').getElementsByTagName('a');
 var imgs = document.getElementById('imgs').getElementsByTagName('img');
 var i = 0;
-for(var i in imglinks) {
-    imglinks[i].addEventListener('click', showImage);
+var selectedImg = imgs[i];
+for(var j in imglinks) {
+    imglinks[j].addEventListener('click', showImage);
 } 
 
 function showImage() {
+    clearInterval(interval);
+    //alert(window.selectedImage + ", " + i);
     if(selectedImg !== undefined) {
         selectedImg.style.display = 'none';
     }
-    selectedImg = document.getElementById(this.getAttribute('data-img')); //this.attributes.data-img,  this.attributes["data-img"]
+    imgid = this.getAttribute('data-img');
+    i = parseInt(imgid.substr(-1));
+    alert(i + ", " + imgid);
+    selectedImg = document.getElementById(imgid); //this.attributes.data-img,  this.attributes["data-img"]
     selectedImg.style.display = 'block';
+    interval = setInterval(changeImg, 1000);
 }
 
-var myVar = setInterval(changeImg, 1000);
+var interval = setInterval(changeImg, 1000);
 function changeImg() {
     // hide the old image
     if(selectedImg !== undefined) {
@@ -41,4 +47,81 @@ function nextImg() {
     selectedImage = imgs[i];
 }
 
+function start() {
+    interval = setInterval(changeImg, 1000);
+}
+//document.addEventListener('load', changeImg);
 
+*/
+
+var i = 0;
+var imglinks = document.getElementById('imglinks').getElementsByTagName('a');
+var imgs = document.getElementById('imgs').getElementsByTagName('img');
+var selectedImg = imgs[0];
+var ani = true;
+// when click in the link i, the image i shows.
+for(var j in imglinks) {
+    imglinks[j].addEventListener('mouseover', showImg);
+    imglinks[j].addEventListener('mouseout', start);
+    imglinks[j].addEventListener('click', switchAnim);
+    //imglinks[j].addEventListener('click', showImg);
+} 
+
+function switchAnim() {
+   ani = !ani; 
+}
+
+function showImg() {
+    //console.log(i);
+    //console.log(selectedImg.getAttribute('id'));
+    clearTimeout(timer);
+    clearInterval(interval);
+    timer = false;
+    selectedImg.style.display ='none';
+    i = parseInt(this.getAttribute('data-img'));//parseInt()
+    //console.log(i);
+    selectedImg = imgs[i];
+    //console.log(selectedImg.getAttribute('id'));
+    selectedImg.style.display ='block';
+    //console.log(i);
+    //console.log(selectedImg.getAttribute('id'));
+}
+
+var timer, interval;
+function start() {
+    if(ani && !timer) {
+        timer = setTimeout(startInt, 1000)
+    }
+}
+function startInt() {
+    interval = setInterval(changeImg, 1000);
+}
+function changeImg() {
+    // hide the old image
+    console.log(i);
+    console.log(selectedImg.getAttribute('id'));
+    selectedImg.style.display = 'none';
+    // find the new image
+    i = cycle();
+    selectedImg = imgs[i];
+    console.log(i);
+    console.log(selectedImg.getAttribute('id'));
+    // show the new image
+    selectedImg.style.display ='block';
+}
+
+function cycle() {
+    if(i == imgs.length-1) {
+       return 0;
+    }
+    else {
+        return i + 1;
+    } 
+}
+/*
+function times(t) {
+    if(i == 0) {
+        i ++;
+    }
+    return i*t;
+}*/
